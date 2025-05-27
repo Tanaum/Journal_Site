5779b33affcb4b526b80b6bc8a77d0df2f2eee55
Tanaum Masooma <135850652+Tanaum@users.noreply.github.com>
2025-05-27 18:21:09 +0500

    previous entry page and js code
    what i did:
    -deleted any unnecessary code from the script
    -made a new html file that will be used to display all the previous diary entries
    -made a function to sort and also display each entry chronologically
    
    to be done:
    -make it good to look at
    -made the script more readable

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
 //VARIABLES
+
+//TO TAKE THE CONTENT FROM THE USER
 var diaryEntry = $('.diaryEntry');
 var diaryEntryButton = $('.diaryEntryButton');
 
-//const keys = [-1]; idt imma need this later now lawlz
+var prvEntries = $('.prvEntries');
 
+//stores all of the keys so that they can be arranged
+const timestampKeys = [];
 
 // EVENT LISTNER
-diaryEntryButton.on("click", testButton);
 
-//FUNCTION
-function testButton(){
+//TO TAKE THE CONTENT FROM THE USER
+diaryEntryButton.on("click", saveDiaryContent);
+
+// On document ready, only display entries if the container exists (PrvEnt.html)
+$(document).ready(function() {
+    if ($('.prvEntries').length) {
+        displayPreviousEntries();
+    }
+});
+
+//FUNCTIONS
+
+//SAVING CONTENT GIVEN BY THE USER
+function saveDiaryContent(){
     //DATE
     var d = new Date()
     var date = d.getDate();
     var month = d.getMonth()+1;
     var year = d.getFullYear();
 
-    var entryIndex = localStorage.getItem("entryIndex") || 0;
-
-    //
+    //DIARY PAGE OBJECT
     const diaryPage = {
         todaysDate : `${date}/${month}/${year}`,
         diaryContent : diaryEntry.val()
     };
 
-    localStorage.setItem("entryIndex", Number(entryIndex)+1);
-    localStorage.setItem(`${Date.now()}`,JSON.stringify(diaryPage));
+    if (diaryPage.diaryContent !== ""){
+        localStorage.setItem(`${Date.now()}`,JSON.stringify(diaryPage));
+    }
+
+    diaryEntry.val(''); // Clear the textarea after submission
 };
+
+function displayPreviousEntries(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        timestampKeys.push(Number(key));
+    }
+
+    // Sort keys ascending (oldest to newest)
+    timestampKeys.sort((a, b) => a - b);
+
+    // Now loop through in order
+    timestampKeys.forEach(keyNum => {
+        let diaryEntry = JSON.parse(localStorage.getItem(keyNum.toString()));
+    //  alert(`${diaryEntry.todaysDate}: ${diaryEntry.diaryContent}`);
+        
+        var entryHTML = `
+            <div class="diaryentryBox">
+                <p><strong>${diaryEntry.todaysDate}:</strong></p>
+                <p>${diaryEntry.diaryContent}</p>
+            </div>
+        `;
+
+        prvEntries.append(entryHTML);
+    });
+}
